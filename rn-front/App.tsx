9c7cb62dcb7668a5a22682b0a9d94f560cb502a9
hi629 <hirojoy189@gmail.com>
2021-07-11 21:46:51 +0900

    🔥 不要箇所削除

--- rn-front/App.tsx
+++ rn-front/App.tsx
@@ -1,38 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { View, FlatList } from "react-native";
-import styled from "styled-components/native";
 import AddInput from "./components/AddMemo";
 import MemoList from "./components/MemoList";
 import axios from "axios";
 import { Memo } from "./types/memo";
-
-const ComponentContainer = styled.View`
-	flex-direction: row;
-`;
-
-const InputContainer = styled.View`
-	flex-direction: row;
-	border-radius: 10px;
-`;
-
-const Input = styled.TextInput`
-	font-size: 20px;
-	background-color: white;
-	width: 300px;
-	margin-right: 20px;
-	padding: 10px;
-	margin-bottom: 20px;
-	border-radius: 10px;
-`;
-
-const SubmitButton = styled.TouchableOpacity`
-	width: 50px;
-	justify-content: center;
-	align-items: center;
-	background-color: whitesmoke;
-	margin-bottom: 20px;
-	border-radius: 50px;
-`;
 
 export default function App() {
 	const [data, setData] = useState();
